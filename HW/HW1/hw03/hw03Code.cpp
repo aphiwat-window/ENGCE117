69b3dfff193c39de71b766a2cc7fd3af7a6df523
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-23 11:51:34 +0700

    Update hw03Code.cpp

--- HW/HW1/hw03/hw03Code.cpp
+++ HW/HW1/hw03/hw03Code.cpp
@@ -1,1 +1,28 @@
+/*
+    เขียนโปรแกรมรับ "ชื่อ" จากผู้ใช้ และแสดงผลบนหน้าจอดังผลลัพธ์ด้านล่างต่อไปนี้ 
+    
+    Test case:
+        First Name: Aomsin
+        Last Name: AomMoney
+    Output:
+        Aomsin AomMoneys TC, RMUTL, Chiang Mai, Thailand
 
+    Test case:
+        First Name: ABC
+        Last Name: DEF
+    Output:
+        ABD DEFs TC, RMUTL, Chiang Mai, Thailand
+*/
+
+#include <stdio.h>
+
+int main () {
+    char Fistname[100] , Lastname[100] ;
+    printf( "First Name: " ) ;
+    scanf( "%s" , Fistname ) ;
+
+    printf( "Last Name: " ) ;
+    scanf( "%s" , Lastname ) ;
+
+    printf("\n%s %s TC, RMUTL, Chiang Mai, Thailand" , Fistname , Lastname ) ;
+}//end main
