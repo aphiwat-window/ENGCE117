a51c162a6c1f6f36c8b57fe2bf96d3e5a28b4035
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-23 23:25:58 +0700

    Update hw04-3.cpp

--- HW/HW4/hw04-3/hw04-3.cpp
+++ HW/HW4/hw04-3/hw04-3.cpp
@@ -1,1 +1,20 @@
+/*
+    จงกรอกหมายเลขทั้งหมดจำนวน 3 ค่า และแสดงผลรวมดังแสดงดังผลลัพธ์ด้านล่าง
+    
+    Test case:
+        2 3 1
+    Output:
+        Answer = 6
 
+    Test case:
+        4 -1 3
+    Output:
+        Answer = 6
+*/
+#include <stdio.h>
+int main() {
+    int a = 0 , b = 0 , c = 0 ;
+    printf( "กรอกค่า a b c : " ) ;
+    scanf( "%d %d %d" , &a , &b , &c ) ;
+    printf( "Answer = %d" , a + b + c) ;
+}//end main
