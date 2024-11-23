de4b3921277eee1a8e3a0d329c53fce4148fa886
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-24 00:23:18 +0700

    Update hw04-5.cpp

--- HW/HW4/hw04-5/hw04-5.cpp
+++ HW/HW4/hw04-5/hw04-5.cpp
@@ -1,1 +1,24 @@
+/*
+    จงเขียนโปรแกรมเพื่อแปลงหน่วยจาก "วัน" เป็น "วินาที"
+    
+    Test case:
+        Input Days : 
+            8
+    Output:
+        8 days = 691200 seconds
 
+    Test case:
+        Input Days : 
+            27
+    Output:
+        27 days = 2332800 seconds
+*/
+
+#include <stdio.h>
+
+int main () {
+    int Day = 0 ;
+    printf( "Input Days : " ) ;
+    scanf( "%d" , &Day ) ;
+    printf( "%d days = %d" , Day , Day * 24 * 60 * 60 ) ;
+}//end main
