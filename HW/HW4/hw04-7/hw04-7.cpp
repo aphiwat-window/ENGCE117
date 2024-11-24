9e9b536cf8c8becfb1c75431ae56ce5849d43265
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-24 19:40:58 +0700

    Update hw04-7.cpp

--- HW/HW4/hw04-7/hw04-7.cpp
+++ HW/HW4/hw04-7/hw04-7.cpp
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    float tem = 0 ;
+
+    printf( "Enter temperature : " ) ;
+    scanf( "%f" , &tem ) ;
+
+    printf( "%.0f degree Celsius = %.1f  degree Fahrenheit" , tem , tem * 1.8 + 32 ) ;
+    return 0 ;
+}//end main
