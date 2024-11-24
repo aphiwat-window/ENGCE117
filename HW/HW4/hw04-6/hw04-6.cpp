0ec65ea67dd12eeea03e6c19c0e4f920ecb41595
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-24 16:33:45 +0700

    Update hw04-6.cpp

--- HW/HW4/hw04-6/hw04-6.cpp
+++ HW/HW4/hw04-6/hw04-6.cpp
@@ -1,1 +1,66 @@
+/*
+    เขียนโปรแกรมภาษาซีเพื่อรับข้อมูลพนักงานของบริษัทซอร์ฟแวร์ โดยรับข้อมูล รหัสประจำตัวพนักงาน , จำนวนชั่วโมงที่ทำงาน , รายได้ต่อชั่วโมง จากนั้นให้แสดงข้อมูลเลขประจำตัวพนักงาน พร้อมกับรายได้ทั้งหมดที่หนักงานจะได้รับทั้งหมด
+    (ผลลัพธ์ของ Salary จะต้องอยู่ในรูปแบบ Decimal Number เท่านั้น เช่น 374,000)
 
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0342
+        Input the working hrs: 
+            8
+        Salary amount/hr: 
+            15000
+    Output:
+        Expected Output:
+        Employees ID = 0342
+        Salary = U$ 120,000.00
+y8uin 
+    Test case:
+        Input the Employees ID(Max. 10 chars): 
+            0000500349
+        Input the working hrs: 
+            11
+        Salary amount/hr: 
+            34000
+    Output:
+        Expected Output:
+        Employees ID = 0000500349
+        Salary = U$ 374,000.00
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main () {    
+    int hour = 0 ;
+    float Salary_amount = 0 , Salary = 0 ;
+    char ID[ 11 ] , Convert[ 100 ] ;
+    
+    printf( "Input the Employees ID(Max. 10 chars): " ) ;
+    scanf( "%s" , ID ) ;
+    printf( "Input the working hrs: " ) ;
+    scanf( "%d" , &hour) ;
+    printf( "Salary amount/hr: " ) ;
+    scanf( "%f" , &Salary_amount ) ;
+
+    printf( "Employees ID = " ) ;
+    for ( int i = 0 ; i < 10 ; i++ ) {
+        printf( "%c" , ID[ i ]) ;
+    }//end for print id
+
+    printf( "\n" ) ;
+
+    Salary = Salary_amount * hour ; //คำนวน
+    sprintf( Convert , "%.2f" , Salary ) ; //แปลงเป็น string
+    int length = strlen( Convert ) ; //วัดความยาว
+ 
+    printf( "Salary = U$ " ) ;  
+    for (int i = 0 ; i < length - 3 ; i++ ) { //กำหนดว่าจะพิมกี่ตัว
+        if (i != 0 && ( length - i ) % 3 == 0 ) { //กำหนดว่าถ้าครบ 3 ตัวให้พิม ,
+            printf( "," ) ;
+        }
+        printf( "%c" , Convert[ i ] ) ; 
+    } 
+    printf( "%s\n" , strchr( Convert, '.' ) ) ; //พิมข้อความทั้งหมดหลังจากทศนิยม
+
+    return 0 ;
+}//end main
