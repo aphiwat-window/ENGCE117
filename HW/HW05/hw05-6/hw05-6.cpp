a87487b747c0dca58fefa8a6ec9acf54c3e4479d
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-25 22:19:22 +0700

    Update hw05-6.cpp

--- HW/HW05/hw05-6/hw05-6.cpp
+++ HW/HW05/hw05-6/hw05-6.cpp
@@ -1,1 +1,38 @@
+/*
+    จงรับค่าจากผู้ใชิ และวิเคราะห์ว่าจำนวนดังกล่าวเป็นจำนวนเต็มบวก หรือจำนวนเต็มลบ
+    
+    Test case:
+        Input :
+            49
 
+    Output:
+        Positive
+
+    Test case:
+        Input :
+            -90
+
+    Output:
+        Negative
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int input = 0 ;
+
+    printf( "Input :\n" ) ;
+    scanf( "%d" , &input ) ;
+
+    printf( "\nOutput:\n" ) ;
+    if ( input > 0) { 
+        printf( "Positive" ) ;
+    } else if ( input < 0 ) {
+        printf( "Negative" ) ;
+    } else {
+        printf( "Zero" ) ;
+    }//end if ตรวจเช็ค
+    
+    return 0 ;
+}//end main
