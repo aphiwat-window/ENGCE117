96ec754275811ea3b9e1ab8956d12b14917f7fe7
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-11-25 12:22:15 +0700

    Update hw05-1.cpp

--- HW/HW05/hw05-1/hw05-1.cpp
+++ HW/HW05/hw05-1/hw05-1.cpp
@@ -1,1 +1,29 @@
+/*
+    รับตัวอักขระมาทั้งหมด 4 ค่า แล้วนำมาแสดงผลย้อนกลับ เช่น กรอกค่า L M X Y ต้องแสดงเป็น Y X M L เป็นต้น
+    
+    Test case:
+        L M Y K
+    Output:
+        Result: K Y M L
 
+    Test case:
+        A B C D
+    Output:
+        Result: D C B A
+*/
+
+#include <stdio.h>
+
+int main()  {
+    char ch1, ch2 , ch3 , ch4 ;
+    printf( "Enter character " ) ;
+    scanf( "%c %c %c %c" , &ch1 , &ch2 , &ch3 , &ch4 ) ;
+
+    if ( ch1 && ch2 && ch3 && ch4 ) { //เช็คว่ามีค่าหรือไม่
+        printf( "%c %c %c %c" , ch4 , ch3 , ch2 , ch1 ) ;
+    } else {
+        printf( "invaild" ) ;
+    }//end if else
+    
+    return 0 ;
+}//end main
