fa7e2f92fa0e41f4b6c188a020ac5de467975a75
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-01 01:45:43 +0700

    Update hw09-4.cpp

--- HW/HW9/hw09-4/hw09-4.cpp
+++ HW/HW9/hw09-4/hw09-4.cpp
@@ -1,1 +1,104 @@
+/*
+    ผู้ใช้กรอกจำนวนอาเรย์ที่ต้องการกรอก จากนั้นผู้ใช้ทำการกรอกข้อมูลลงไปในอาเรย์จนครบ และให้คุณแสดงจำนวนชุดตัวเลขที่ซ้ำกันจากที่ผู้ใช้กรอก
+    
+    Test case:
+        Input N :
+            4
+        Element[0] :
+            3
+        Element[1] :
+            3
+        Element[2] :
+            2
+        Element[3] :
+            5
+    Output:
+        2 -> 1 value.
+        3 -> 2 values.
+        5 -> 1 value.
 
+
+    Test case:
+        Input N :
+            9
+        Element[0] :
+            6
+        Element[1] :
+            6
+        Element[2] :
+            5
+        Element[3] :
+            9
+        Element[4] :
+            1
+        Element[5] :
+            9
+        Element[6] :
+            7
+        Element[7] :
+            6
+        Element[8] :
+            2
+    Output:
+        1 -> 1 value.
+        2 -> 1 value.
+        5 -> 1 value.
+        6 -> 3 values.
+        7 -> 1 value.
+        9 -> 2 values.
+*/
+#include <stdio.h>
+
+int main() {
+
+    int input[100] , input2[100] , n = 0 , count = 0 , output[100] , swap = 0 ; 
+
+    printf( "Enter N : \n" ) ;
+    scanf( "%d" , &n ) ;
+
+    //------------------------------------------------//
+
+    for ( int i = 0 ; i < n ; i++ ) {
+        printf( "Element [%d]\n" , i ) ;
+        scanf( "%d" , &input[i] ) ;
+    }//end for รับค่า
+      for ( int i = 0 ; i < n - 1 ; i++ ) {
+        for ( int j = 0 ; j < n - i - 1 ; j++ ) {
+            if ( input[j] > input[j + 1] ) { 
+                swap = input[j] ;
+                input[j] = input[j + 1] ;
+                input[j + 1] = swap ; 
+            } //end if น้อยไปมาก
+        }//end if สลับ
+    }//end if จำนวนรอบ
+    for ( int i = 0 ; i < n ; i++ ) {
+        input2[i] = input[i] ;
+    }//end for cpy input1 ไป input2
+
+    //------------------------------------------------//
+    
+    printf( "\n" ) ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        count = 0 ;
+        if ( input[i] == -1 ) {  // ถ้าค่าถูกนับแล้ว
+            continue ;
+        } for ( int j = 0 ; j < n ; j++ ) {
+            if ( input[i] == input2[j] ) {
+                count ++  ;
+                input2[j] = -1 ;
+            }//end if ถ้าเจอซ้ำ
+        }//end for เช็คขวา
+        output[i] = count ;
+    }//end for เช็คซ้าย
+    
+    //------------------------------------------------//
+
+    printf( "Output : \n" ) ;
+    for ( int i = 0 ; i < n; i++ ) {
+        if ( output[i] == 0 ) {  
+            continue ;
+        }//end if ถ้าเจอไม่มีข้อมูลให้ข้าม
+        printf( "%d -> %d\n" , input[i] , output[i] ) ;
+    }//end for แสดงค่า
+    return 0 ;
+}//end main
