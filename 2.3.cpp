4ff2a6d1c4bb2682224dfc71c7c8c17bfc79645f
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-01 14:40:10 +0700

    Update 2.3.cpp

--- 2.3.cpp
+++ 2.3.cpp
@@ -6,12 +6,17 @@ void explode( const char str1[] , const char splitter[] , char str2[][10] , int 
 int main() {
     char out[20][10] ;
     int num = 0 ;
-    explode( "asdasd/adsa/sda" , "/" , out , &num ) ;//เรียกใช้งาน
+    explode( "" , "/" , out , &num ) ;//เรียกใช้งาน
 
     for ( int i = 0 ; i < num ; i++ ) {
        printf( "str2[%d] = \"%s\"\n" , i , out[i] ) ; 
     }//end for แสดงค่า
-    printf( "count = %d" , num ) ;
+    if ( num != 0 ) {
+        printf( "count = %d" , num ) ;
+    } else {
+        printf( "count = 0" ) ; 
+    }//end แสดงผล
+    
     return 0 ;
 }//end function
 
@@ -19,13 +24,9 @@ void explode( const char str1[] , const char splitter[] , char str2[][10] , int 
     char data[20] ;
     strcpy( data , str1 ) ;
     char *token = strtok( data , splitter ) ; //ตรวจว่าเจอตัวแบ่งตัวไหน
-    if ( token == NULL ) { 
-        printf( "count = 0" ) ; 
-    } else {
-        while ( token != NULL ) {
+    while ( token != NULL ) {
             strcpy( str2[*count] , token ) ; //ก็อปใส่ str2
             ( *count ) ++ ;
             token = strtok( NULL , splitter ) ; 
         }//end แยกคำ
-    }//end if เช็คว่ามีตัวอักษรไหม
 }//end explode
