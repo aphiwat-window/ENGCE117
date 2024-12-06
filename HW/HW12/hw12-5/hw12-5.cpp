59de48dba3f9a5616401b42626a7f6eb877c1b31
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-06 20:23:33 +0700

    Update hw12-5.cpp

--- HW/HW12/hw12-5/hw12-5.cpp
+++ HW/HW12/hw12-5/hw12-5.cpp
@@ -1,1 +1,118 @@
+/*
+    HR ของบริษัทเน็ตเวิร์คทำการกรอกข้อมูลพนักงานลงไปในระบบคำนวณฐานภาษีเพื่อเตรียมส่งยอดภาษีสู่ฝ่ายบัญชี โดยจะหักภาษี ณ ที่จ่าย 7% ของเงินเดือนที่ได้ ให้คุณคำนวณภาษีพนักงานทั้งหมดที่บริษัทของคุณต้องชำระภายในปีนี้ (กรอกเลข -1 ถือเป็นการสิ้นสุดการกรอก และทำการสรุปผลภาษีที่ต้องชำระภายในปีนี้) และสร้างฟังก์ชันที่ระบุคนที่ต้องชำระภาษีเยอะที่สุดในบริษัทมาแสดงในผลสรุปยอดของบริษัทด้วย
 
+    Test case:
+        Employee 1's Name : 
+            Omar
+        Sarary (Bath/Month) : 
+            12750
+        Employee 2's Name : 
+            Alice
+        Sarary (Bath/Month) : 
+            15900
+        Employee 3's Name : 
+            Max
+        Sarary (Bath/Month) : 
+            19860
+        Employee 4's Name : 
+            -1
+    Output:
+        All salary per month: 48,510.00 Bath
+        All salary per year: 582,120.00 Bath
+        Tax (7% per year) : 40,748.40 Bath
+        Most tax in company : Max (16,682.40 Bath per year)
+
+    Test case:
+        Employee 1's Name : 
+            John
+        Sarary (Bath/Month) : 
+            15780
+        Employee 2's Name : 
+            Oven
+        Sarary (Bath/Month) : 
+            16890
+        Employee 3's Name : 
+            May
+        Sarary (Bath/Month) : 
+            17870
+        Employee 4's Name : 
+            Bill
+        Sarary (Bath/Month) : 
+            22100
+        Employee 5's Name : 
+            -1
+    Output:
+        All salary per month: 72,640.00 Bath
+        All salary per year: 871,680.00 Bath
+        Tax (7% per year) : 61,017.60 Bath
+        Most tax in company : Bill (18,564.00 Bath per year)
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+struct Person {
+	char Name[20] ;
+    float Salary ;
+} ;
+
+struct Person User[ 100 ] ;
+
+float most_salary = -9999 , sum_month , sum_year , Tax , Most_tax ;
+char Most_name[ 100 ] ;
+int i = 0 ;
+
+void cal( ) ;
+void sort ( float data) ;
+
+int main () {
+    while ( 1 ) {
+        printf( "Employee [%d] Name : " , i + 1 ) ;
+        scanf( "%s" , User[ i ].Name ) ;
+        if ( strcmp( User[i].Name , "-1" ) == 0 ) {
+            break ;
+        }//end if ถ้ากรอก -1
+        printf( "Sarary (Bath/Month) : " ) ;
+        scanf( "%f" , &User[ i ].Salary ) ;
+        printf( "------------------------\n" ) ;
+        sum_month += User[ i ].Salary ;
+        if ( User[ i ].Salary > most_salary ) {
+            most_salary = User[ i ].Salary ;
+            strcpy( Most_name , User[i].Name ) ; 
+        }//end หาใครเงินเยอะสุด
+        i++ ;
+    }//end while
+    cal( ) ;
+    return 0 ;
+}//end main
+
+void cal( ) {
+    printf( "Output:\nAll salary per month: " ) ;
+    sort( sum_month ) ;
+    printf( " Bath\n" ) ;
+    sum_year = sum_month * 12 ;
+    printf( "All salary per year: " ) ;
+    sort( sum_year ) ;
+    printf( " Bath\n" ) ;
+    Tax = sum_year * 0.07 ;
+    printf( "Tax (7%% per year) : " ) ;
+    sort( Tax ) ;
+    printf( " Bath\n" ) ;
+    most_salary = most_salary * 12 * 0.07 ;
+    printf( "Most tax in company : %s (" , Most_name ) ;
+    sort( most_salary ) ;
+    printf( " Bath per year)\n" ) ;
+}//end cal
+
+void sort ( float data ) {
+    char Convert[ 100 ] ;
+    sprintf( Convert , "%.2f" , data ) ;
+    int length = strlen( Convert ) ; //วัดความยาว
+    for ( int i = 0 ; i < length - 3 ; i++ ) { //กำหนดว่าจะพิมกี่ตัว
+        if ( i != 0 && ( length - i ) % 3 == 0 ) { //กำหนดว่าถ้าครบ 3 ตัวให้พิม ,
+            printf( "," ) ;
+        }//end if
+        printf( "%c" , Convert[ i ] ) ; 
+    }//end for
+    printf( "%s" , strchr( Convert, '.' ) ) ; //พิมข้อความทั้งหมดหลังจากทศนิยม
+}//end sort
