82fdb098f8da176d8d72a236c9aec426b41e2b72
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-09 00:17:52 +0700

    Update hw15-3.cpp

--- HW/HW15/hw15-3/hw15-3.cpp
+++ HW/HW15/hw15-3/hw15-3.cpp
@@ -1,1 +1,30 @@
+/*
+    อ่านไฟล์และนำมาแสดงผลข้อมูลบนหน้าจอ
 
+    ข้อมูลภายในไฟล์ a15-3.txt :
+        Somchai	19	3.78	Male
+        Somjing	18	3.54	Female
+        Somjung	20	2.44	Male
+    Output:
+        Somchai	19	3.78	Male
+        Somjing	18	3.54	Female
+        Somjung	20	2.44	Male
+*/
+
+#include <stdio.h>
+
+int main () {
+    FILE *fp ;
+    fp = fopen( "a15-3.txt" , "r" ) ;
+    if ( fp == NULL ) {
+        printf("ERR") ;
+        return 0 ;
+    }//end if err
+
+    float Grade , Age ;
+    char Name[ 20 ] , Sex[ 20 ] ;
+    while ( fscanf( fp , "%s %f %f %s" , Name , &Age , &Grade , Sex ) != EOF ) {
+        printf( "%s  %.0f  %.2f  %s\n" , Name , Age , Grade , Sex ) ;
+    }//end while scan and print
+    return 0 ;
+}//end main
