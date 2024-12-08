0c5fd0ae55a38ed97cd235b93af3f68b7ac26613
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-08 23:10:03 +0700

    Update hw15-1.cpp

--- HW/HW15/hw15-1/hw15-1.cpp
+++ HW/HW15/hw15-1/hw15-1.cpp
@@ -1,1 +1,27 @@
+/*
+    จงเขียนโปรแกรมเพื่ออ่านค่าจากไฟล์ a15-1.txt แล้วนำมาแสดงผลบนโปรแกรม
 
+    ข้อมูลภายในไฟล์ a.txt
+        Hello World
+    
+    Output:
+        Text in file = Hello World
+*/
+
+#include <stdio.h>
+
+int main () {
+    FILE *fp ;
+    fp = fopen( "a15-1.txt" , "r" ) ;
+    if ( fp == NULL ) {
+        printf("ERR") ;
+        return 0 ;
+    }//end if err
+
+    char name[ 200 ] ;
+    printf( "Text in file = " ) ;
+    while ( fgets( name , sizeof( name ) , fp ) != NULL ) {
+        printf( "%s" , name ) ;
+    }//end while print
+    return 0 ;
+}//end main
