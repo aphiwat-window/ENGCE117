50f20d327c31423ecb35a63338a53dc3be69f09f
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-14 19:56:44 +0700

    Update hw16-1.cpp

--- HW/HW16/hw16-1/hw16-1.cpp
+++ HW/HW16/hw16-1/hw16-1.cpp
@@ -1,1 +1,30 @@
+/*
+    จงเขียนโปรแกรมเพื่อสร้างฟังก์ชัน SequentialSearch ให้สมบูรณ์
 
+    Test case:
+
+    Output:
+        Found in 3
+
+*/
+
+#include <stdio.h>
+
+int SequentialSearch( int data[], int n, int find ) ;
+
+int main() {
+    int RealData[ 6 ] = { 7, 4, 1, 8, 3, 2 } ;
+    printf( "Found in %d\n", SequentialSearch( RealData, 6, 8 ) ) ;
+    return 0 ;
+}//end function
+
+int SequentialSearch( int data[], int n, int find ) {
+    int Output = -1 ;
+    for ( int i = 0 ; i < n ; i++ ) {
+        if (data[ i ] == find ) {
+            Output = i ;
+            break;
+        }//end if
+    }//end for
+    return Output ;
+}//end SequentialSearch
