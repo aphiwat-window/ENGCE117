5fc9119b5deee7c790b1f5c02cb7b69dd8fd9d0e
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-14 20:03:48 +0700

    Update hw07-4.cpp

--- HW/HW7/hw07-4/hw07-4.cpp
+++ HW/HW7/hw07-4/hw07-4.cpp
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int isPrime( int num ) {
+    int i = 2 ;
+    int prime = 1 ;
+
+    while ( i * i <= num ) {
+        prime = prime * ( num % i != 0 ) ; 
+        i++ ;
+    }
+    return prime ;
+}
+
+int main() {
+    int num ;
+
+    printf( "Enter number :\n" ) ;
+    scanf( "%d" , &num ) ;
+
+    int i = num ;
+    do {
+        int prime = isPrime( i ) ;
+        while  ( prime ) {
+            printf( "%d " , i ) ;
+            prime = 0 ;
+        }//end do while
+        i-- ;
+    } while ( i >= 2 ) ;
+    printf( "\n" ) ;
+    return 0 ;
+}//end main
