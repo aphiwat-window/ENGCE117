ab27c7015845e43cc8cbeee9c7f72ede29cdb147
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-14 20:06:42 +0700

    Update hw07-4.cpp

--- HW/HW7/hw07-4/hw07-4.cpp
+++ HW/HW7/hw07-4/hw07-4.cpp
@@ -1,3 +1,20 @@
+/*
+    รับค่าจากผู้ใช้จำนวน 1 ค่า และให้แสดงจำนวนเฉพาะตั้งแต่ค่าแรก จนถึงค่าที่ผู้ใช้กรอก โดยแสดงจำนวนเฉพาะจาก "มาก ไปหา น้อย"
+    (โจทย์ข้อนี้ให้ใช้ Do While Loop เท่านั้น)
+    
+    Test case:
+        Enter number :
+            10
+    Output:
+        7 5 3 2
+
+    Test case:
+        Enter number :
+            100
+    Output:
+        97 89 83 79 73 71 67 61 59 53 47 43 41 37 31 29 23 19 17 13 11 7 5 3 2
+*/
+
 #include <stdio.h>
 
 int isPrime( int num ) {
@@ -14,10 +31,11 @@ int isPrime( int num ) {
 int main() {
     int num ;
 
-    printf( "Enter number :\n" ) ;
+    printf( "Enter number : " ) ;
     scanf( "%d" , &num ) ;
 
     int i = num ;
+    printf( "Output:\n" ) ;
     do {
         int prime = isPrime( i ) ;
         while  ( prime ) {
