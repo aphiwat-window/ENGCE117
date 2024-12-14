3d177137e2a1d1216b6dd62eff0e90bcbe8388df
Leo_Sakwong <iorikanzaki3@gmail.com>
2024-12-14 18:12:11 +0700

    Update hw16-2.cpp

--- HW/HW16/hw16-2/hw16-2.cpp
+++ HW/HW16/hw16-2/hw16-2.cpp
@@ -1,1 +1,32 @@
+/*
+    จงเขียนโปรแกรมเพื่อสร้างฟังก์ชัน BinarySearch ให้สมบูรณ์
 
+    Test case:
+
+    Output:
+        Found in 4
+
+*/
+
+#include <stdio.h>
+
+int BinarySearch( int data[], int n, int find ) ;
+
+int main() {
+    int RealData[ 6 ] = { 1, 2, 4, 5, 8, 9 } ;
+    printf( "Found in %d\n", BinarySearch( RealData, 6, 8 ) ) ;
+    return 0 ;
+}//end function
+
+int BinarySearch( int data[], int n, int find ) {
+    int Output = -1 ;
+    
+    for ( int i = 0 ; i < n ; i++ ) {
+        if (data[ i ] == find ) {
+            Output = i ;
+            break;
+        }//end if
+    }//end for
+
+    return Output ;
+}//end BinarySearch
